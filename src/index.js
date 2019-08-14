47a6fbc3d946cdc16faf6ded3da2efd39becfe1f
Julie Thrall <juliethrall10@gmail.com>
2019-08-14 15:59:23 -0700

    connected header to the store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { featureReducer } from './reducers';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+export const store = createStore(featureReducer);
+console.log('this is the store', store);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
